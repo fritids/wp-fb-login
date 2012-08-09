42906c9ea7135de6a70e2de4b353021d21f8e603
Jacob Perez <philweb.programmer88@gmail.com>
2012-08-09 14:12:27 -0700

    update fb_login_process.php and inc.wp.php

---
+++ inc.wp.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file tries to include the Wordpress header by searching UP the directory structure
+ */
+$searchFile = 'wp-blog-header.php';
+
+for($i = 0; $i < 10; $i++) {
+    if(file_exists($searchFile)) {
+        require_once($searchFile);
+        break;    
+    }
+    $searchFile = "../" . $searchFile;
+}
+
+//Make sure we got it
+if(! defined('WPINC')) {
+    die("Failed to locate wp-blog-header.php.");
+}
+
+//Include the User Registration code so we can use wp_insert_user
+if(! function_exists('wp_insert_user'))
+    require_once(ABSPATH . WPINC . '/registration.php');
+    
+if(! function_exists('wp_insert_user')) {
+    die("Failed to include registration.php.");
+}
